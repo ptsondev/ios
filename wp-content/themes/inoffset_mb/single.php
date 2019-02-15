3c365df713992ada852a46783c5ce7f28d5cced1
root <root@bepankhang.vinahost.vn>
2019-02-14 21:57:17 -0500

    before newver

--- wp-content/themes/inoffset_mb/single.php
+++ wp-content/themes/inoffset_mb/single.php
@@ -9,11 +9,7 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article itemscope itemtype="http://schema.org/CreativeWork" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 id="site-title" class="entry-title" itemprop="about"><?php the_title(); ?></h1>			
-                    <div id="post-more-info">
-                        <span class="vcard updated"><?php the_date('d/m/Y'); ?></span> - 
-                        <span class="vcard author author_name"><span class="fn">In Offset HCM</span></span>
-                    </div>
+					<h1 id="site-title" class="entry-title" itemprop="about"><?php the_title(); ?></h1>			                    
 					<div  class="entry-content" itemprop="text">
 						<?php the_content(); ?>
 					</div>					
